4cdebd295ed5323dd3e9717afa1daa9f2db6ac30
Peter Baran <piotrbaran@hotmail.com>
2023-09-21 13:22:31 -0600

    Added a test box

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+
+// Create the Large Square containing the grid
+const largeSquare = document.createElement("div");
+largeSquare.className = "large-square";
+document.body.appendChild(largeSquare);
+console.log("hello");
