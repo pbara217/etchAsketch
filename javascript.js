eddba213cf156fe7868781b857e226ad660bbc21
Peter Baran <piotrbaran@hotmail.com>
2023-09-21 14:13:37 -0600

    completed grid styling

--- javascript.js
+++ javascript.js
@@ -8,19 +8,27 @@ largeSquare.className = "large-square";
 const wrapper = document.querySelector(".wrapper");
 wrapper.appendChild(largeSquare);
 
-// Create a smaller square in the grid
-const smallSquare = document.createElement("div");
-smallSquare.className = "small-square";
 
-// Append the small Square in hte large square
-largeSquare.appendChild(smallSquare);
 
 //Get the size of the grid
-const largeSquareSize = 500;
+const largeSquaredim = 500;
 const divide = 16;
-const smallSquareSize = largeSquareSize / divide;
+const smallSquareSize = largeSquaredim / divide;
+const largeSquareSize = largeSquaredim + (2*divide);
 
-//Set dimension of the small squares
+//Size large grid
+largeSquare.style.width = `${largeSquareSize}px`;
+largeSquare.style.height = `${largeSquareSize}px`;
+
+
+
+// Create the X*X amount of sqaures
+
+for(let i = 0; i < (divide * divide); i++) {
+    const smallSquare = document.createElement("div");
+smallSquare.className = "small-square";
 smallSquare.style.width = `${smallSquareSize}px`;
 smallSquare.style.height = `${smallSquareSize}px`;
-
+largeSquare.appendChild(smallSquare);
+    console.log(i)
+}
