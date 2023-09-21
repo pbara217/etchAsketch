6bbb7eaa3a7fc6c7dae2f64e9d1a474da48d8ac8
Peter Baran <piotrbaran@hotmail.com>
2023-09-21 13:51:11 -0600

    Got styling set up and set up test grid

--- javascript.js
+++ javascript.js
@@ -3,5 +3,14 @@
 // Create the Large Square containing the grid
 const largeSquare = document.createElement("div");
 largeSquare.className = "large-square";
-document.body.appendChild(largeSquare);
-console.log("hello");
+
+//Append to the web section
+const wrapper = document.querySelector(".wrapper");
+wrapper.appendChild(largeSquare);
+
+// Create a smaller square in the grid
+const smallSquare = document.createElement("div");
+smallSquare.className = "small-square";
+
+// Append the small Square in hte large square
+largeSquare.appendChild(smallSquare);
