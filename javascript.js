e0c7ca769f6f1235480051a52395ad8efd27b189
Peter Baran <piotrbaran@hotmail.com>
2023-09-21 14:46:26 -0600

    Added button to re-size grid

--- javascript.js
+++ javascript.js
@@ -38,3 +38,46 @@ smallSquare.onmouseover = function(){
 }
 
 // Change over in the size
+const sizebutton = document.getElementById("sizePrompt");
+sizebutton.addEventListener("click", function (){
+    let input = prompt("Please enter the amount of squares");
+
+//Clear our the large square
+const largeSquare = document.querySelector(".large-square");
+largeSquare.innerHTML = "";
+
+//Recalculate size
+
+const largeSquaredim = 500;
+let divide = input;
+const smallSquareSize = largeSquaredim / divide;
+const largeSquareSize = largeSquaredim + (2*divide);
+
+//Size large grid
+largeSquare.style.width = `${largeSquareSize}px`;
+largeSquare.style.height = `${largeSquareSize}px`;
+
+
+
+// Create the X*X amount of sqaures
+
+for(let i = 0; i < (divide * divide); i++) {
+    const smallSquare = document.createElement("div");
+smallSquare.className = "small-square";
+smallSquare.style.width = `${smallSquareSize}px`;
+smallSquare.style.height = `${smallSquareSize}px`;
+largeSquare.appendChild(smallSquare);
+
+// Add the hoverover event
+smallSquare.onmouseover = function(){
+    this.style.backgroundColor = "black";
+};
+}
+
+
+
+
+});
+
+
+
